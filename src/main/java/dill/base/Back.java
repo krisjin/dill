ff9e2bd80388d20ca98c71537a36c59cff85c93b
krisjin <krisibm@163.com>
2019-10-29 07:01:12 +0800

    update

---
+++ src/main/java/dill/base/Back.java
@@ -0,0 +1,42 @@
+package dill.base;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author krisijn on 2019/10/28
+ */
+public class Back {
+
+
+    public void dfs(String pre, String str, List<String> res, int index) {
+        if (index == str.length())
+            res.add(pre);
+        else {
+            char ch = str.charAt(index);
+            if (!Character.isLetter(ch))
+                dfs(pre + ch, str, res, index + 1);
+            else {
+                // 小写字符分支
+                ch = Character.toLowerCase(ch);
+                dfs(pre + ch, str, res, index + 1);
+                // 大写字符分支
+                ch = Character.toUpperCase(ch);
+                dfs(pre + ch, str, res, index + 1);
+            }
+        }
+    }
+
+    public List<String> letterCasePermutation(String str) {
+        List<String> res = new LinkedList<>();
+        dfs("", str, res, 0);
+        return res;
+    }
+
+    // 测试
+    public static void main(String[] args) {
+        String S = "a1b";
+        Back to784To = new Back();
+        System.out.println(to784To.letterCasePermutation(S));
+    }
+}
