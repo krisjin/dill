83772253562c2d796676da7564bddf2a23c141d3
krisjin <krisibm@163.com>
2019-10-31 06:51:03 +0800

    update

--- src/main/java/dill/leetcode/C0283.java
+++ src/main/java/dill/leetcode/C0283.java
@@ -2,8 +2,6 @@ package dill.leetcode;
 
 /**
  * @author krisjin on 2019/10/15
- * <p>
- * <p>
  * 移动零
  * 链接：https://leetcode-cn.com/problems/move-zeroes
  * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
