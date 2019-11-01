0c455caeb68d99ec3264d8ccbc28eb2078431c63
krisjin <krisibm@163.com>
2019-11-01 23:38:51 +0800

    update

--- src/main/java/dill/leetcode/C0860.java
+++ src/main/java/dill/leetcode/C0860.java
@@ -52,4 +52,40 @@ package dill.leetcode;
  * </pre>
  */
 public class C0860 {
+
+
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int bill : bills) {
+            if (bill == 5)
+                five++;
+            else if (bill == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            } else {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+//        int[] bills = {5, 5, 5, 10, 20};
+        int[] bills = {5,5,10,10,20};
+
+        C0860 c0860 = new C0860();
+
+        System.err.println(c0860.lemonadeChange(bills));
+    }
+
 }
