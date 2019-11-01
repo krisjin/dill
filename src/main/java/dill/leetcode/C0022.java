b4c8aba2adf66e555f355da637c70f01760d7758
krisjin <krisibm@163.com>
2019-11-01 14:44:44 +0800

    update

--- src/main/java/dill/leetcode/C0022.java
+++ src/main/java/dill/leetcode/C0022.java
@@ -1,5 +1,8 @@
 package dill.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * User:krisjin
  * Date:2019-10-29
@@ -23,4 +26,69 @@ package dill.leetcode;
  * </pre>
  */
 public class C0022 {
+
+    //1.递归
+
+    public List<String> generateParenthesis(int n) {
+        List<String> combinations = new ArrayList();
+        generateAll(new char[2 * n], 0, combinations);
+        return combinations;
+    }
+
+
+    public void generateAll(char[] current, int pos, List<String> result) {
+        if (pos == current.length) {
+            if (valid(current))
+                result.add(new String(current));
+        } else {
+            current[pos] = '(';
+            generateAll(current, pos + 1, result);
+
+            current[pos] = ')';
+            generateAll(current, pos + 1, result);
+        }
+    }
+
+
+    public boolean valid(char[] current) {
+        int balance = 0;
+        for (char c : current) {
+            if (c == '(') balance++;
+            else balance--;
+            if (balance < 0) return false;
+        }
+        return (balance == 0);
+    }
+
+
+    //2.回溯
+
+    public List<String> generateParenthesis2(int n) {
+        List<String> ans = new ArrayList();
+        backtrack(ans, "", 0, 0, n);
+        return ans;
+    }
+
+    public void backtrack(List<String> ans, String cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            ans.add(cur);
+            return;
+        }
+
+        if (open < max)
+            backtrack(ans, cur + "(", open + 1, close, max);
+        if (close < open)
+            backtrack(ans, cur + ")", open, close + 1, max);
+    }
+
+
+    public static void main(String[] args) {
+
+        C0022 c0022 = new C0022();
+
+        List<String> dd = c0022.generateParenthesis2(3);
+
+        System.out.println(dd);
+
+    }
 }
