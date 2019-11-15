6a2b014f66a3ae76360da08a415fba0e612a7e75
krisjin <krisibm@163.com>
2019-11-15 21:21:02 +0800

    add

--- src/main/java/dill/leetcode/C0062.java
+++ src/main/java/dill/leetcode/C0062.java
@@ -1,5 +1,7 @@
 package dill.leetcode;
 
+import java.util.Arrays;
+
 /**
  * User:krisjin
  * Date:2019-11-14
@@ -36,4 +38,50 @@ package dill.leetcode;
  * </pre>
  */
 public class C0062 {
+
+    public static int uniquePaths1(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < n; i++) dp[0][i] = 1;
+        for (int i = 0; i < m; i++) dp[i][0] = 1;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+
+    public static int uniquePaths2(int m, int n) {
+        int[] pre = new int[n];
+        int[] cur = new int[n];
+        Arrays.fill(pre, 1);
+        Arrays.fill(cur, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                cur[j] = cur[j - 1] + pre[j];
+            }
+            pre = cur.clone();
+        }
+        return pre[n - 1];
+    }
+
+
+    public static int uniquePaths3(int m, int n) {
+        int[] cur = new int[n];
+        Arrays.fill(cur, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                cur[j] += cur[j - 1];
+            }
+        }
+        return cur[n - 1];
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+
 }
