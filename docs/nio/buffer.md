f0867e6ca5ff3b372539a2ae6a9eaaeb7ff37dee
krisjin <krisibm@163.com>
2020-08-03 09:42:29 +0800

    update nio test

---
+++ docs/nio/buffer.md
@@ -0,0 +1,19 @@
+
+
+
+
+Buffer limit的作用，设置此缓冲区的限制，缓冲区的限制代表第一个不应该读取或写入元素的index
+
+
+
+
+
+Clear方法的侧重点在于还原一切
+
+rewind
+
+flip
+
+limit
+
+position
--- src/main/java/dill/nio/BufferTest.java
+++ src/main/java/dill/nio/BufferTest.java
@@ -8,7 +8,7 @@ import java.nio.CharBuffer;
  * User:krisjin
  * Date:2020-08-01
  */
-public class BufferDemo {
+public class BufferTest {
 
     public static void main(String[] args) {
         createBuffer();
