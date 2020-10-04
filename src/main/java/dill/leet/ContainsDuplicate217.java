319b05f44981e2e195dbd8e73b02fe8850f76abe
krisjin <krisibm@163.com>
2020-10-04 16:18:47 +0800

    clean

--- src/main/java/dill/leet/ContainsDuplicate217.java
+++ src/main/java/dill/leet/ContainsDuplicate217.java
@@ -1,4 +1,4 @@
-package dill.base.array;
+package dill.leet;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -17,24 +17,21 @@ import java.util.Set;
  * Date: 2015/9/17
  * Time: 14:32
  */
-public class ContainsDuplicate {
+public class ContainsDuplicate217 {
 
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<Integer>();
         for (int i : nums) {
-            if (set.contains(i)) {
-                return true;
-            } else {
-                set.add(i);
-            }
+            if (set.contains(i)) return true;
+            else set.add(i);
         }
         return false;
     }
 
     public static void main(String[] args) {
-        int[] arr = {2, 3, 4, 5, 2,6, 7, 1};
-        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
-        boolean ret = containsDuplicate.containsDuplicate(arr);
+        int[] arr = {2, 3, 4, 5, 2, 6, 7, 1};
+        ContainsDuplicate217 containsDuplicate217 = new ContainsDuplicate217();
+        boolean ret = containsDuplicate217.containsDuplicate(arr);
         System.out.println(ret);
     }
 }
