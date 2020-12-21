9b1eb8e6defcba54efe9eb8028fe1e7e6011b49a
krisjin <krisibm@163.com>
2020-12-21 15:03:45 +0800

    update config

--- src/main/java/dill/base/math/ReverseInteger.java
+++ src/main/java/dill/base/math/ReverseInteger.java
@@ -1,5 +1,7 @@
 package dill.base.math;
 
+import java.util.Stack;
+
 /**
  * 难度：简单
  * <p/>
@@ -12,14 +14,13 @@ package dill.base.math;
  * 再对数值除以10，得到一个整数数进行下次取模加值。
  * User : krisibm@163.com
  * Date: 2015/9/17
- * Time: 17:32
  */
 public class ReverseInteger {
 
     public int reverse(int x) {
         int res = 0;
         while (x != 0) {
-            // 每一次都在原来结果的基础上变大10倍，再加上余数
+            // 每一次都在原来结果的基础上变大10倍，再加上余数 x%10，总是会获取最后一个数
             res = res * 10 + x % 10;
             //对x不停除10
             x = x / 10;
@@ -28,10 +29,54 @@ public class ReverseInteger {
         return res;
     }
 
+    /**
+     * 暴力方法
+     *
+     * @param x
+     * @return
+     */
+    public static String reverse2(int x) {
+        String strX = x + "";
+        String result = "";
+        for (int i = strX.length(); i > 0; i--) {
+            result += strX.charAt(i - 1);
+        }
+        return result;
+    }
+
+    /**
+     * 使用stack,代码很复杂
+     *
+     * @param x
+     * @return
+     */
+    public static String reverse3(int x) {
+        String strX = x + "";
+        String negative = "";
+        if (strX.indexOf("-") != -1) {
+            negative = "-";
+            strX = strX.substring(1);
+        }
+        Stack<Character> stack = new Stack();
+
+        String result = negative;
+        for (int i = 0; i < strX.length(); i++)
+            stack.push((strX.charAt(i)));
+
+        for (int j = 0; j < strX.length(); j++) {
+            result += stack.pop();
+        }
+        return result;
+    }
+
+
     public static void main(String[] args) {
         ReverseInteger reverseInteger = new ReverseInteger();
-        int ret = reverseInteger.reverse(-4532);
-        System.out.println(ret);
+        int ret = reverseInteger.reverse(0);
+//        System.out.println(ret);
+        System.out.println(reverse2(-123));
+
+        System.out.println(reverse3(-123));
     }
 
 }
