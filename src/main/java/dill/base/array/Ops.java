e9da28395c43099b3ccf9a69c82581d89d1ce835
krisjin <krisibm@163.com>
2021-04-25 22:52:09 +0800

    test

--- src/main/java/dill/base/array/Ops.java
+++ src/main/java/dill/base/array/Ops.java
@@ -5,13 +5,19 @@ package dill.base.array;
  * @Date 2020/9/24
  */
 public class Ops {
-
-    public static int maxVal(int[] nums) {
-        int max = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > max) max = nums[i];
+    /**
+     * 数组求最大值
+     * 使用一个地址，存储最大值。迭代遍历数组，迭代中的每个元素值都与该最大值进行比较，当num[i] 大于maxVal，执行maxVal = numArr[i]
+     *
+     * @param numArr
+     * @return
+     */
+    public static int maxVal(int[] numArr) {
+        int maxVal = 0;
+        for (int i = 0; i < numArr.length; i++) {
+            if (numArr[i] > maxVal) maxVal = numArr[i];
         }
-        return max;
+        return maxVal;
     }
 
     public static double avg(double nums[]) {
