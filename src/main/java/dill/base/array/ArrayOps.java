359ba8da79a53c8a7a7400c6ccad574aa0849c20
krisjin <krisibm@163.com>
2021-04-26 22:39:26 +0800

    test

--- src/main/java/dill/base/array/ArrayOps.java
+++ src/main/java/dill/base/array/ArrayOps.java
@@ -4,7 +4,7 @@ package dill.base.array;
  * @User krisjin
  * @Date 2020/9/24
  */
-public class Ops {
+public class ArrayOps {
     /**
      * 数组求最大值
      * 使用一个地址，存储最大值。迭代遍历数组，迭代中的每个元素值都与该最大值进行比较，当num[i] 大于maxVal，执行maxVal = numArr[i]
@@ -20,31 +20,37 @@ public class Ops {
         return maxVal;
     }
 
-    public static double avg(double nums[]) {
+    /**
+     * @param numArr
+     * @return
+     */
+    public static double avg(double numArr[]) {
         double sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
+        for (int i = 0; i < numArr.length; i++) {
+            sum += numArr[i];
         }
-        double avg = sum / nums.length;
+        double avg = sum / numArr.length;
         return avg;
     }
 
-    public static void reverse(int nums[]) {
-        int N = nums.length;
+    public static void reverse(int numArr[]) {
+        int N = numArr.length;
         for (int i = 0; i < N / 2; i++) {
-            int temp = nums[i];
-            nums[i] = nums[N - 1 - i];
-            nums[N - 1 - i] = temp;
+            int temp = numArr[i];
+            numArr[i] = numArr[N - 1 - i];
+            numArr[N - 1 - i] = temp;
         }
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        int ret = maxVal(nums);
+        int[] numArr = {1, 2, 3, 4, 5, 6, 7};
+        int ret = maxVal(numArr);
         System.out.println(ret);
 
-        reverse(nums);
-        for (int i = 0; i < nums.length; i++)
-            System.out.print(nums[i] + " ");
+        reverse(numArr);
+        for (int i = 0; i < numArr.length; i++)
+            System.out.print(numArr[i] + " ");
     }
+
+
 }
