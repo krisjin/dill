9af0304aa46ec88fe79788f1cc8bd03a6aa62368
krisjin <krisibm@163.com>
2021-05-07 21:43:39 +0800

    更新两数之和的思路

--- src/main/java/dill/lee/TwoSum1.java
+++ src/main/java/dill/lee/TwoSum1.java
@@ -4,13 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * User:krisjin
- * Date:2019-10-10
  * 两数之和
  * https://leetcode-cn.com/problems/two-sum
  * <pre>
- *
- * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 给定一个整数数组 nums和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
  *
  * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
  *
@@ -25,8 +22,12 @@ import java.util.Map;
  * 标。输出是有要求的，1， 坐标较小的放在前面，较大的放在后面。2， 这俩坐标不能为零。
  * 时间复杂度: O(N)******- 空间复杂度: O(N)
  * </pre>
+ * 思路：通过给出的已知条件：1.给定的数组中存在两个数相加等于目标值，要想求出求出数组中两个数的和等于目标值，
+ * 可以在遍历数组时，先求出另一个值(目标值减当前遍历值)，并将该值存在map中。求出另一个值后，没次要去map中查找该值，如果有那就直接返回了。没有就将当前遍历的值存如map中。key为元素值，value为元素索引
+ *
  */
 public class TwoSum1 {
+
     Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 
     public int[] twoSum(int[] nums, int target) {
