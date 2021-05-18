ea6ddbe110bb3efeaf3c14ccac354bf16895c75a
krisjin <krisibm@163.com>
2021-05-18 08:18:50 +0800

    test

--- src/main/java/dill/lee/RemoveElement27.java
+++ src/main/java/dill/lee/RemoveElement27.java
@@ -1,11 +1,10 @@
 package dill.lee;
 
+import java.util.Arrays;
+
 /**
- * 在一个数组里面移除指定value，并且返回新的数组长度。这题唯一需要注意的地方在于，不能新建另一个数组
- * 重新给指定数组赋值
- *
  * <pre>
- * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+ *     给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
  *
  * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
  *
@@ -13,31 +12,44 @@ package dill.lee;
  *
  *  
  *
- * 示例 1:
+ * 说明:
  *
- * 给定 nums = [3,2,2,3], val = 3,
+ * 为什么返回数值是整数，但输出的答案是数组呢?
  *
- * 函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+ * 请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
  *
- * 你不需要考虑数组中超出新长度后面的元素。
- * 示例 2:
+ * 你可以想象内部操作如下:
  *
- * 给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+ * // nums 是以“引用”方式传递的。也就是说，不对实参作任何拷贝
+ * int len = removeElement(nums, val);
  *
- * 函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+ * // 在函数里修改输入数组对于调用者是可见的。
+ * // 根据你的函数返回的长度, 它会打印出数组中 该长度范围内 的所有元素。
+ * for (int i = 0; i < len; i++) {
+ *     print(nums[i]);
+ * }
+ *  
  *
- * 注意这五个元素可为任意顺序。
+ * 示例 1：
  *
- * 你不需要考虑数组中超出新长度后面的元素。
+ * 输入：nums = [3,2,2,3], val = 3
+ * 输出：2, nums = [2,2]
+ * 解释：函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。你不需要考虑数组中超出新长度后面的元素。例如，函数返回的新长度为 2 ，而 nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案。
+ * 示例 2：
  *
- * 来源：力扣（LeetCode）
- * https://leetcode-cn.com/problems/remove-element
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 输入：nums = [0,1,2,2,3,0,4,2], val = 2
+ * 输出：5, nums = [0,1,4,0,3]
+ * 解释：函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。注意这五个元素可为任意顺序。你不需要考虑数组中超出新长度后面的元素。
+ *  
+ *
+ * 提示：
+ *
+ * 0 <= nums.length <= 100
+ * 0 <= nums[i] <= 50
+ * 0 <= val <= 100
+ *
+ * 链接：https://leetcode-cn.com/problems/remove-element
  * </pre>
- * <p/>
- * User : krisibm@163.com
- * Date: 2015/7/1
- * https://leetcode-cn.com/problems/remove-element/
  */
 public class RemoveElement27 {
 
@@ -54,7 +66,7 @@ public class RemoveElement27 {
     public static void main(String[] args) {
         int[] ids = {1, 2, 3, 4, 5, 6, 7, 5, 5};
         int len = removeElement(ids, 5);
-        System.out.println(len);
+        System.out.println(len + "    " + Arrays.toString(ids));
     }
 
 }
