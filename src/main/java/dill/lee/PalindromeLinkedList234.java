038ff6047cb6881600e6245d2d13619c64cde2c3
krisjin <krisibm@163.com>
2021-08-23 09:02:30 +0800

    add valid palindrome

---
+++ src/main/java/dill/lee/PalindromeLinkedList234.java
@@ -0,0 +1,28 @@
+package dill.lee;
+
+import dill.base.ListNode;
+
+/**
+ * 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+ * 示例 1：
+ * 输入：head = [1,2,2,1]
+ * 输出：true
+ *
+ * 示例 2：
+ * 输入：head = [1,2]
+ * 输出：false
+ *
+ * 进阶：你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ *
+ * User: krisjin
+ * Date: 2021/8/20
+ */
+public class PalindromeLinkedList234 {
+
+
+    public boolean isPalindrome(ListNode head) {
+
+        return true;
+    }
+
+}
---
+++ src/test/java/dill/lee/test/T.java
@@ -0,0 +1,63 @@
+package dill.lee.test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * User: krisjin
+ * Date: 2021/7/7
+ */
+public class T {
+    public static void main(String[] args) {
+        User u1 = new User("2021-06-04", 1);
+        User u2 = new User("2021-06-05", 1);
+        User u3 = new User("2021-06-06", 1);
+
+
+        List<User> userList = new ArrayList<>();
+        List<User> userList2 = new ArrayList<>();
+        userList.add(u1);
+        userList.add(u2);
+        userList.add(u3);
+
+
+        userList2.add(new User("11", 11));
+
+        userList.addAll(userList2);
+
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date targetDt = null;
+        try {
+            targetDt = sdf.parse("2021-06-05");
+            Iterator<User> userIterator = userList.iterator();
+            while (userIterator.hasNext()) {
+                User u = userIterator.next();
+                Date date = sdf.parse(u.dt);
+                if (date.getTime() < targetDt.getTime()) {
+                    userIterator.remove();
+                }
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        System.err.println(userList.size());
+    }
+
+    static class User {
+        public String dt;
+        public Integer id;
+
+
+        public User(String dt, Integer id) {
+            this.dt = dt;
+            this.id = id;
+        }
+
+
+    }
+}
