a8caeef4745bb6cd2804f8c295a2f7f25458017c
krisjin <krisibm@163.com>
2021-08-30 08:10:17 +0800

    test

--- src/main/java/dill/lee/ContainsDuplicateII.java
+++ src/main/java/dill/lee/ContainsDuplicateII.java
@@ -1,8 +1,28 @@
 package dill.lee;
 
 /**
+ * https://leetcode-cn.com/problems/contains-duplicate-ii
+ * <pre>
+ * 给定一个整数数组和一个整数k，判断数组中是否存在两个不同的索引i和j，使得nums [i] = nums [j]，并且 i 和 j的差的 绝对值 至多为 k。
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [1,2,3,1], k = 3
+ * 输出: true
+ *
+ * 示例 2:
+ * 输入: nums = [1,0,1,1], k = 1
+ * 输出: true
+ *
+ * 示例 3:
+ * 输入: nums = [1,2,3,1,2,3], k = 2
+ * 输出: false
+ *
+ * </pre>
  * User: krisjin
  * Date: 2021/8/30
  */
 public class ContainsDuplicateII {
+
+
 }
