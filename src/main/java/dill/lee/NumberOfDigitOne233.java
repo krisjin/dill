16b51b1465c69a76a8d3c98d6ddd5038ed4dd1b6
krisjin <krisibm@163.com>
2021-09-01 08:49:59 +0800

    countDigitOne

---
+++ src/main/java/dill/lee/NumberOfDigitOne233.java
@@ -0,0 +1,26 @@
+package dill.lee;
+
+/**
+ * 数字1的个数
+ * https://leetcode-cn.com/problems/number-of-digit-one/
+ * <pre>
+ * 给定一个整数 n，计算所有小于等于 n 的非负整数中数字 1 出现的个数。
+ *
+ * 示例 1：
+ * 输入：n = 13
+ * 输出：6
+ * 示例 2：
+ *
+ * 输入：n = 0
+ * 输出：0
+ * </pre>
+ * User: krisjin
+ * Date: 2021/9/1
+ */
+public class NumberOfDigitOne233 {
+
+    public int countDigitOne(int n) {
+
+        return 1;
+    }
+}
