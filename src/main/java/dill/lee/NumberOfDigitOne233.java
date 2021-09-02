c66ebbdd2d98c1c569720b13f98c77ba9bd9f40f
krisjin <krisibm@163.com>
2021-09-02 08:58:50 +0800

    test1

--- src/main/java/dill/lee/NumberOfDigitOne233.java
+++ src/main/java/dill/lee/NumberOfDigitOne233.java
@@ -14,13 +14,19 @@ package dill.lee;
  * 输入：n = 0
  * 输出：0
  * </pre>
- * User: krisjin
- * Date: 2021/9/1
  */
 public class NumberOfDigitOne233 {
 
     public int countDigitOne(int n) {
-
-        return 1;
+        // mulk 表示 10^k
+        // 在下面的代码中，可以发现 k 并没有被直接使用到（都是使用 10^k）
+        // 但为了让代码看起来更加直观，这里保留了 k
+        long mulk = 1;
+        int ans = 0;
+        for (int k = 0; n >= mulk; ++k) {
+            ans += (n / (mulk * 10)) * mulk + Math.min(Math.max(n % (mulk * 10) - mulk + 1, 0), mulk);
+            mulk *= 10;
+        }
+        return ans;
     }
 }
