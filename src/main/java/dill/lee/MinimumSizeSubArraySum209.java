a6198bbf761bb3084766b75428834a23159b8b3f
krisjin <krisibm@163.com>
2021-09-03 08:40:27 +0800

    minSubArrayLen

---
+++ src/main/java/dill/lee/MinimumSizeSubArraySum209.java
@@ -0,0 +1,34 @@
+package dill.lee;
+
+/**
+ * 长度最小的子数组
+ * https://leetcode-cn.com/problems/minimum-size-subarray-sum/
+ * <pre>
+ *  给定一个含有n个正整数的数组和一个正整数 target 。
+ *
+ * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组[numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+ *
+ * 示例 1：
+ *
+ * 输入：target = 7, nums = [2,3,1,2,4,3]
+ * 输出：2
+ * 解释：子数组[4,3]是该条件下的长度最小的子数组。
+ * 示例 2：
+ *
+ * 输入：target = 4, nums = [1,4,4]
+ * 输出：1
+ * 示例 3：
+ *
+ * 输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+ * 输出：0
+ *
+ * </pre>
+ */
+public class MinimumSizeSubArraySum209 {
+
+
+    public int minSubArrayLen(int target, int[] nums) {
+        return 0;
+    }
+
+}
