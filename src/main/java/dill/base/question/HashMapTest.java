f00dfe532b95917760d7950ff07622e866994b0d
krisjin <krisibm@163.com>
2021-09-16 22:50:01 +0800

    test

--- src/main/java/dill/base/question/HashMapTest.java
+++ src/main/java/dill/base/question/HashMapTest.java
@@ -11,19 +11,10 @@ public class HashMapTest {
 
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
-
-
         map.put("xx", "xxx");
-
-
         String str = "x";
         int h;
-
         int code = (str == null) ? 0 : (h = str.hashCode()) ^ (h >>> 16);
-
-
         System.out.println(16 << 3);
-
-
     }
 }
