8e492d0e49f84d4a3f739ba3d27932818c1a075e
krisjin <krisibm@163.com>
2021-09-19 00:44:40 +0800

    fun

--- src/main/java/dill/lee/MergeTwoSortedList21.java
+++ src/main/java/dill/lee/MergeTwoSortedList21.java
@@ -3,14 +3,13 @@ package dill.lee;
 import dill.base.ListNode;
 
 /**
- * @author krisjin on 2019/10/18
  * 合并两个有序链表
  * https://leetcode-cn.com/problems/merge-two-sorted-lists/solution/
  * <pre>
- *   将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
- *   示例：
- *   输入：1->2->4, 1->3->4
- *   输出：1->1->2->3->4->4
+ *  将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ *  示例：
+ *  输入：1->2->4, 1->3->4
+ *  输出：1->1->2->3->4->4
  * </pre>
  */
 public class MergeTwoSortedList21 {
