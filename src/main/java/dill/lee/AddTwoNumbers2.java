5570a7d73d52b57ef12fe8a02a84d77022f1b343
krisjin <krisibm@163.com>
2022-01-29 09:51:24 +0800

    clean

--- src/main/java/dill/lee/AddTwoNumbers2.java
+++ src/main/java/dill/lee/AddTwoNumbers2.java
@@ -7,7 +7,6 @@ import dill.base.ListNode;
  * https://leetcode-cn.com/problems/add-two-numbers
  * <pre>
  * 给出两个非空的链表用来表示两个非负的整数。其中，它们各自的位数是按照逆序的方式存储的，并且它们的每个节点只能存储一位数字。
- *
  * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
  *
  * 您可以假设除了数字 0 之外，这两个数都不会以 0开头。
@@ -17,20 +16,24 @@ import dill.base.ListNode;
  * 输出：7 -> 0 -> 8
  * 原因：342 + 465 = 807
  * </pre>
+ *
  */
 public class AddTwoNumbers2 {
 
     /**
-     * 思路 1 ******- 时间复杂度: O(N)******- 空间复杂度: O(1)******
-     * 迭代，每次只算个位数的相加
+     *
      *
      * @param l1
      * @param l2
      * @return
      */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
         ListNode head = new ListNode(0);
         ListNode p = head;
 
