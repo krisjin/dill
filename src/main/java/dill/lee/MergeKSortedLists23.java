c53b0af6448b1ed1053c0c0cea3d14ae65261673
krisjin <krisibm@163.com>
2022-03-31 23:37:47 +0800

    test

--- src/main/java/dill/lee/MergeKSortedLists23.java
+++ src/main/java/dill/lee/MergeKSortedLists23.java
@@ -2,7 +2,12 @@ package dill.lee;
 
 import dill.base.ListNode;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
+ * https://leetcode-cn.com/problems/merge-k-sorted-lists
+ * <p>
  * 给你一个链表数组，每个链表都已经按升序排列。
  * <p>
  * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
@@ -40,14 +45,59 @@ import dill.base.ListNode;
  * lists[i] 按 升序 排列
  * lists[i].length 的总和不超过 10^4
  * <p>
- * 链接：https://leetcode-cn.com/problems/merge-k-sorted-lists
  */
 public class MergeKSortedLists23 {
 
+    //解法1
+    public ListNode mergeKLists_1(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(lists.length, new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                if (o1.val < o2.val) return -1;
+                else if (o1.val == o2.val) return 0;
+                else return 1;
+            }
+        });
+        ListNode dummy = new ListNode(0);
+        ListNode p = dummy;
+        for (ListNode node : lists) {
+            if (node != null) queue.add(node);
+        }
+        while (!queue.isEmpty()) {
+            p.next = queue.poll();
+            p = p.next;
+            if (p.next != null) queue.add(p.next);
+        }
+        return dummy.next;
+    }
+
+
+    //解法
 
     public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        return merge(lists, 0, lists.length - 1);
+    }
 
-        return null;
+    private ListNode merge(ListNode[] lists, int left, int right) {
+        if (left == right) return lists[left];
+        int mid = left + (right - left) / 2;
+        ListNode l1 = merge(lists, left, mid);
+        ListNode l2 = merge(lists, mid + 1, right);
+        return mergeTwoLists(l1, l2);
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
     }
 
 }
