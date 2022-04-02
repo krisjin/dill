6f8c98f8bf9ffca27057bd894528ca823ce9894b
krisjin <krisibm@163.com>
2022-04-02 22:51:06 +0800

    test

--- src/main/java/dill/lee/MergeKSortedLists23.java
+++ src/main/java/dill/lee/MergeKSortedLists23.java
@@ -7,14 +7,10 @@ import java.util.PriorityQueue;
 
 /**
  * https://leetcode-cn.com/problems/merge-k-sorted-lists
- * <p>
  * 给你一个链表数组，每个链表都已经按升序排列。
- * <p>
  * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
- * <p>
  *  
  * 示例 1：
- * <p>
  * 输入：lists = [[1,4,5],[1,3,4],[2,6]]
  * 输出：[1,1,2,3,4,4,5,6]
  * 解释：链表数组如下：
@@ -25,12 +21,12 @@ import java.util.PriorityQueue;
  * ]
  * 将它们合并到一个有序链表中得到。
  * 1->1->2->3->4->4->5->6
- * 示例 2：
  * <p>
+ * 示例 2：
  * 输入：lists = []
  * 输出：[]
- * 示例 3：
  * <p>
+ * 示例 3：
  * 输入：lists = [[]]
  * 输出：[]
  *  
@@ -108,6 +104,15 @@ public class MergeKSortedLists23 {
 
 
     public static void main(String[] args) {
+
+        ListNode node1 = new ListNode(2);
+        node1.next = new ListNode(3);
+        node1.next.next = new ListNode(5);
+
+        ListNode node2 = new ListNode(3);
+        node2.next = new ListNode(6);
+        node2.next.next = new ListNode(9);
+
 
     }
 }
