aa857a7838d36de3ade9099e158f98b4145b6ef2
krisjin <krisibm@163.com>
2022-04-11 22:52:40 +0800

    test

--- src/main/java/dill/lee/MergeKSortedLists23.java
+++ src/main/java/dill/lee/MergeKSortedLists23.java
@@ -49,11 +49,14 @@ public class MergeKSortedLists23 {
     public static ListNode mergeKLists_1(ListNode[] lists) {
         if (lists == null || lists.length == 0) return null;
         PriorityQueue<ListNode> queue = new PriorityQueue<>(lists.length, new Comparator<ListNode>() {
-            @Override
             public int compare(ListNode o1, ListNode o2) {
-                if (o1.val < o2.val) return -1;
-                else if (o1.val == o2.val) return 0;
-                else return 1;
+                if (o1.val < o2.val) {
+                    return -1;
+                } else if (o1.val == o2.val) {
+                    return 0;
+                } else {
+                    return 1;
+                }
             }
         });
         ListNode dummy = new ListNode(0);
@@ -121,12 +124,11 @@ public class MergeKSortedLists23 {
 
         ListNode result = mergeKLists_1(nodeList.toArray(new ListNode[2]));
 
-        while (result !=null) {
-            System.out.print(result.val+" ");
+        while (result != null) {
+            System.out.print(result.val + " ");
             result = result.next;
 
         }
-
 
     }
 }
