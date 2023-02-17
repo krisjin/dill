7db28a79d20e113ab5c04a5dbcf9b339ebba1179
krisjin <krisibm@163.com>
2023-02-17 22:53:49 +0800

    洗衣服脏了，不开心

--- src/main/java/dill/base/sort/BubbleSort.java
+++ src/main/java/dill/base/sort/BubbleSort.java
@@ -2,8 +2,11 @@ package dill.base.sort;
 
 
 /**
+ * //
  * 人生态度，言如意，调整，真意
  * 比较，一次次的向上飘移
+ * <p>
+ * //
  */
 public final class BubbleSort {
 
@@ -25,23 +28,6 @@ public final class BubbleSort {
             }
         }
     }
-
-    public static void aa(int nums[]) {
-
-        int len = nums.length;
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < len - 1; j++) {
-                if (nums[j] < nums[j] + 1) {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-
-                }
-            }
-        }
-
-    }
-
 
     public static void main(String[] args) {
         int[] nums = {55, 6, 5, 3, 1, 8, 7, 2, 4, 12};
