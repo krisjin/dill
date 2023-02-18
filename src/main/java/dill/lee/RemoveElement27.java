418291613a63856594cdbea65b44d1b06a1d1c4d
krisjin <krisibm@163.com>
2023-02-18 18:03:08 +0800

    热稀饭，忘了关火，稀饭全烧成炭了，所有的事物高温加热都化为虚无

--- src/main/java/dill/lee/RemoveElement27.java
+++ src/main/java/dill/lee/RemoveElement27.java
@@ -40,7 +40,6 @@ import java.util.Arrays;
  * 输入：nums = [0,1,2,2,3,0,4,2], val = 2
  * 输出：5, nums = [0,1,4,0,3]
  * 解释：函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。注意这五个元素可为任意顺序。你不需要考虑数组中超出新长度后面的元素。
- *  
  *
  * 提示：
  *
@@ -53,14 +52,21 @@ import java.util.Arrays;
  */
 public class RemoveElement27 {
 
+    /**
+     * 不能修改原地址，那只能覆盖，使用新的索引地址记录，一个点
+     *
+     * @param nums
+     * @param val
+     * @return
+     */
     public static int removeElement(int[] nums, int val) {
-        int j = 0;
+        int newIdx = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                nums[j++] = nums[i];
+                nums[newIdx++] = nums[i];
             }
         }
-        return j;
+        return newIdx;
     }
 
     public static void main(String[] args) {
