f3ae176c9d8c9ff4a0b289297b47f257168297f8
krisjin <krisibm@163.com>
2023-02-18 17:08:25 +0800

    有点迷茫了，哎，干点什么呢

--- src/main/java/dill/lee/array/SumOfThree15.java
+++ src/main/java/dill/lee/array/SumOfThree15.java
@@ -36,26 +36,30 @@ import java.util.List;
 public class SumOfThree15 {
 
 
+    /**
+     * @param nums
+     * @return
+     */
     public static List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList();
         int len = nums.length;
         if (nums == null || len < 3) return ans;
         Arrays.sort(nums); // 排序
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < len; i++) { //第一层循环 i
             if (nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // 相邻去重
-            int L = i + 1;
-            int R = len - 1;
-            while (L < R) {
-                int sum = nums[i] + nums[L] + nums[R];
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // i去重,相邻数据相同跳过
+            int l = i + 1; //左指针 i+1
+            int r = len - 1; //右指针 len-1
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
                 if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[L], nums[R]));
-                    while (L < R && nums[L] == nums[L + 1]) L++; // 去重
-                    while (L < R && nums[R] == nums[R - 1]) R--; // 去重
-                    L++;
-                    R--;
-                } else if (sum < 0) L++;
-                else if (sum > 0) R--;
+                    ans.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (l < r && nums[l] == nums[l + 1]) l++; // 去重
+                    while (l < r && nums[r] == nums[r - 1]) r--; // 去重
+                    l++;
+                    r--;
+                } else if (sum < 0) l++;
+                else if (sum > 0) r--;
             }
         }
         return ans;
