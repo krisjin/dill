3bbb3440df1b674b96ce613007782413f29e863b
krisjin <krisibm@163.com>
2023-02-18 08:06:45 +0800

    早上乱七八糟的心情

--- src/main/java/dill/lee/array/ContainerWithMostWater11.java
+++ src/main/java/dill/lee/array/ContainerWithMostWater11.java
@@ -3,30 +3,32 @@ package dill.lee.array;
 /**
  * 盛最多水的容器
  * https://leetcode-cn.com/problems/container-with-most-water
- * <p>
- * <p>
- * 给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。
- * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
- * 说明：你不能倾斜容器，且 n 的值至少为 2。
- * <p>
- * 图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
- * 示例:
- * <p>
- * 输入: [1,8,6,2,5,4,8,3,7]
- * 输出: 49
+ * //
+ * 每个点
+ * //
+ * <pre>
+ * 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+ *
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ *
+ * 返回容器可以储存的最大水量。
+ *
+ * 说明：你不能倾斜容器。
+ *
+ * </pre>
  */
 public class ContainerWithMostWater11 {
 
     //暴力法
-    public static int maxArea1(int[] b) {
-        int max = 0;
-        for (int i = 0; i < b.length - 1; ++i) {
-            for (int j = i + 1; j < b.length; ++j) {
-                int area = (j - i) * Math.min(b[i], b[j]);
-                max = Math.max(max, area);
+    public static int maxArea1(int[] nums) {
+        int maxArea = 0;
+        for (int i = 0; i < nums.length - 1; ++i) {
+            for (int j = i + 1; j < nums.length; ++j) {
+                int area = (j - i) * Math.min(nums[i], nums[j]);
+                maxArea = Math.max(maxArea, area);
             }
         }
-        return max;
+        return maxArea;
     }
 
 
@@ -47,8 +49,7 @@ public class ContainerWithMostWater11 {
             maxArea = Math.max(maxArea, Math.min(nums[l], nums[r]) * (r - l));
             if (nums[l] < nums[r])//移动指针，左值小于幼稚
                 l++;
-            else
-                r--;
+            else r--;
         }
         return maxArea;
     }
