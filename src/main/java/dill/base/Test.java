e74bcb65a853e94d0827f47f8d6513390f34b535
krisjin <krisibm@163.com>
2023-02-20 19:56:25 +0800

    前端的问题，真的很坑

--- src/main/java/dill/base/Test.java
+++ src/main/java/dill/base/Test.java
@@ -6,6 +6,18 @@ package dill.base;
  */
 public class Test {
 
+    //反转
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
 
     public static void main(String[] args) {
 
