7439204eb123199b3d282321dfe08f823151248c
krisjin <krisibm@163.com>
2023-03-07 00:40:33 +0800

    coffee

---
+++ src/main/java/dill/lee/tree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,40 @@
+package dill.lee.tree;
+
+import dill.base.TreeNode;
+
+/**
+ * https://leetcode.cn/problems/maximum-depth-of-binary-tree/
+ * <pre>
+ * 给定一个二叉树，找出其最大深度。
+ *
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最大深度 3 。
+ * </pre>
+ */
+public class MaximumDepthOfBinaryTree {
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
