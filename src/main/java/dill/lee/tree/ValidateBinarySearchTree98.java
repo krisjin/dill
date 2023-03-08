93cd932116caf3d6736919896a25445c4738a00e
krisjin <krisibm@163.com>
2023-03-08 23:29:14 +0800

    看见了烟火，没听见音乐，不知此时在何处

---
+++ src/main/java/dill/lee/tree/ValidateBinarySearchTree98.java
@@ -0,0 +1,65 @@
+package dill.lee.tree;
+
+import dill.base.TreeNode;
+
+/**
+ * 验证二叉搜索树
+ * https://leetcode.cn/problems/validate-binary-search-tree/
+ * <pre>
+ * 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+ *
+ * 有效 二叉搜索树定义如下：
+ *
+ * 节点的左子树只包含 小于 当前节点的数。
+ * 节点的右子树只包含 大于 当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [2,1,3]
+ * 输出：true
+ * 示例 2：
+ *
+ *
+ * 输入：root = [5,1,4,null,null,3,6]
+ * 输出：false
+ * 解释：根节点的值是 5 ，但是右子节点的值是 4 。
+ * </pre>
+ */
+public class ValidateBinarySearchTree98 {
+
+
+    public static boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+
+    public static boolean isValidBST(TreeNode node, long lower, long upper) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= lower || node.val >= upper) {
+            return false;
+        }
+        return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+    }
+
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(5);
+
+        TreeNode right = new TreeNode(4);
+        right.left = new TreeNode(3);
+        right.right = new TreeNode(6);
+
+        root.left = new TreeNode(1);
+        root.right = right;
+
+        boolean result = isValidBST(root);
+        System.err.println(result);
+
+    }
+}
