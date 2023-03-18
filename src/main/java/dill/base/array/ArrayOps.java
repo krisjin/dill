78a49970c45a32ed7b078ddfb89ba955ecc74f8d
krisjin <krisibm@163.com>
2023-03-18 16:45:28 +0800

    ....

--- src/main/java/dill/base/array/ArrayOps.java
+++ src/main/java/dill/base/array/ArrayOps.java
@@ -49,12 +49,21 @@ public class ArrayOps {
         }
     }
 
+
+    public static int[] reverseArray(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left++] = arr[right];
+            arr[right--] = temp;
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
         int[] numArr = {1, 2, 3, 4, 5, 6, 7};
-        int ret = maxVal(numArr);
-        System.out.println(ret);
-
-        reverse(numArr);
+        reverseArray(numArr);
         for (int i = 0; i < numArr.length; i++) {
             System.out.print(numArr[i] + " ");
         }
