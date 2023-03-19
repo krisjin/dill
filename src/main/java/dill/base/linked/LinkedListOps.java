91f83079956e80feab316ca03e677377de7dd08d
krisjin <krisibm@163.com>
2023-03-19 13:24:58 +0800

    ....

---
+++ src/main/java/dill/base/linked/LinkedListOps.java
@@ -0,0 +1,21 @@
+package dill.base.linked;
+
+import dill.base.ListNode;
+
+public class LinkedListOps {
+
+
+    public static void insert(ListNode head) {
+
+
+
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+    }
+}
