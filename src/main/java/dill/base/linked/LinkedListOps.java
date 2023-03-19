8d1ed5978ecd62b049da8fb7a2511a73b27ea7c4
krisjin <krisibm@163.com>
2023-03-19 20:19:12 +0800

    没头脑

--- src/main/java/dill/base/linked/LinkedListOps.java
+++ src/main/java/dill/base/linked/LinkedListOps.java
@@ -2,14 +2,36 @@ package dill.base.linked;
 
 import dill.base.ListNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LinkedListOps {
 
 
-    public static void insert(ListNode head) {
+    public static void insert(ListNode head, ListNode newNode) {
+        ListNode tmp = head.next;//待插入前驱节点
+        head.next = newNode;//将前驱节点next 赋值给插入节点
+        newNode.next = tmp;//待插入节点的后继节点设置
+    }
+
+    public static ListNode del(ListNode head, int value) {
+        if (head.val == value) return head.next;
+        ListNode pre = head;
+        ListNode curr = head.next;
+        while (curr != null && curr.val != value) {
+            pre = curr;
+            curr = curr.next;
+        }
+        if (curr != null) pre.next = curr.next;
+        return head;
+    }
 
 
+    public static void traversal(ListNode head) {
+        List<Integer> nodeList = new ArrayList<>();
 
     }
+
 
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
@@ -17,5 +39,7 @@ public class LinkedListOps {
         head.next.next = new ListNode(3);
         head.next.next.next = new ListNode(4);
 
+//        insert(head, new ListNode(5));
+        del(head, 3);
     }
 }
