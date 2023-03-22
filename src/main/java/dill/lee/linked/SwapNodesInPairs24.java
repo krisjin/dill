aecc99460519662947803a8aaca46abedacb78b4
krisjin <krisibm@163.com>
2023-03-22 22:08:07 +0800

    ........

--- src/main/java/dill/lee/linked/SwapNodesInPairs24.java
+++ src/main/java/dill/lee/linked/SwapNodesInPairs24.java
@@ -3,6 +3,7 @@ package dill.lee.linked;
 import dill.base.ListNode;
 
 /**
+ * 两两交换链表中的节点
  * https://leetcode.cn/problems/swap-nodes-in-pairs/
  * <pre>
  * 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
@@ -27,7 +28,7 @@ import dill.base.ListNode;
 public class SwapNodesInPairs24 {
 
     public ListNode swapPairs(ListNode head) {
-        if(head == null || head.next == null){
+        if (head == null || head.next == null) {
             return head;
         }
         ListNode next = head.next;
@@ -41,7 +42,7 @@ public class SwapNodesInPairs24 {
         ListNode pre = new ListNode(0);
         pre.next = head;
         ListNode temp = pre;
-        while(temp.next != null && temp.next.next != null) {
+        while (temp.next != null && temp.next.next != null) {
             ListNode start = temp.next;
             ListNode end = temp.next.next;
             temp.next = end;
